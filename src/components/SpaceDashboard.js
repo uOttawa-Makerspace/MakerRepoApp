780215979e8aa18d0cd3f3f05f92f05c63cf1d7b
Arthur Fetiveau <art.fetiveau@gmail.com>
2021-10-24 19:08:34 -0400

    Added selected to the current space in space dashboard

--- src/components/SpaceDashboard.js
+++ src/components/SpaceDashboard.js
@@ -101,7 +101,7 @@ const SpaceDashboard = () => {
             <div>
                 <select onChange={e => changeSpace(e.target.value)} className="block w-52 text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500">
                     { inSpaceUsers !== null &&  JSON.parse(inSpaceUsers)['space_list'].map((space) => {
-                        return (<option value={space[1]}>{space[0]}</option>)
+                        return (<option value={space[1]} selected={space[1] === JSON.parse(inSpaceUsers)['space']['id']}>{space[0]}</option>)
                     })}
                 </select>
             </div>
