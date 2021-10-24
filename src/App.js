8567535383173e409c22ae8315c91a5423bdbc7d
Arthur Fetiveau <art.fetiveau@gmail.com>
2021-10-24 18:44:42 -0400

    Added a way to check if the user is actually signed in

--- src/App.js
+++ src/App.js
@@ -5,14 +5,35 @@ import {UserContext} from "./contexts/UserContext";
 import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import env_variables from "./env_variables";
 
 function App() {
 
   const [user, setUser] = useState(null);
   const userProvider = useMemo(() => ({ user, setUser }), [user, setUser]);
 
+    const check_signed_in = () => {
+        fetch(`${env_variables.config.api_url}/check_signed_in`, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }}).then(response => response.json().then((data => {
+                console.log(data);
+                return data === {"signed_in": "true"};
+            })
+        )).catch((error) => {
+            console.error(error);
+        });
+    }
+
     useEffect(() => {
-        setUser(JSON.parse(window.localStorage.getItem("user")));
+        if (check_signed_in) {
+            setUser(JSON.parse(window.localStorage.getItem("user")));
+        } else {
+            setUser(null);
+        }
     }, []);
 
     useEffect(() => {
